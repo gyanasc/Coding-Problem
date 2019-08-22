ec7dd3e72385b8c2d57438b39a09b7200a35073e
gyanasc <54355698+gyanasc@users.noreply.github.com>
2019-08-22 08:36:23 +0530

    import to Git
    First changes done on 21 Aug

---
+++ CodingProblem/ProblemSolve.cs
@@ -0,0 +1,190 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingProblem
+{
+    class ProblemSolve
+    {
+        static List<string> keys1 = new List<string>();
+        public static IList<string> GetKeyFromJsonString(string jsonStr)
+        {
+            JObject jsonObj = JObject.Parse(jsonStr);
+            //List<string> keys1 = new List<string>();
+            //foreach (var child in jsonObj.Children())
+            //{
+            //    if (child.HasValues)
+            //    {
+            //        var str = child.;
+            //        keys1.Add(str);
+            //        //var ChildKeys1 = GetKeys(child);
+            //        //var ChildKeys = jsonObj.Properties().Select(p => p.Name).ToList();
+            //    }
+            //}
+
+            var keys = jsonObj.Properties().Select(p => p.Name).ToList();
+            keys1.AddRange(keys);
+            foreach (var key in keys)
+            {
+                var obj = jsonObj[key];
+                if (obj.HasValues)
+                {
+                    GetKeyFromJsonString(jsonObj[key].ToString());
+                }
+            }
+
+            return keys1;
+        }
+
+        public static IList<string> GetKeys(JToken jsonObj)
+        {
+            var ChildKeys = (jsonObj.ToObject<JObject>()).Properties().Select(p => p.Name).ToList();
+            return ChildKeys;
+        }
+
+        public static string AllCharOccurenceCount(string[] lstStr)
+        {
+            var counts = new Dictionary<char, int>();
+            foreach (string str in lstStr)
+            {
+                foreach (char ch in str)
+                {
+                    int count;
+                    counts.TryGetValue(ch, out count);
+                    count++;
+                    counts[ch] = count;
+                }
+            }
+
+            var entries = counts.Select(d =>
+                string.Format("{0}: {1}", d.Key.ToString() == "" ? "\'\'" : d.Key.ToString(), d.Value));
+            return "{" + string.Join(",", entries) + "}";
+            //return entries;
+        }
+
+        public static string[] RemoveDuplicatesHS(string[] s)
+        {
+            HashSet<string> set = new HashSet<string>(s);
+            string[] result = new string[set.Count];
+            set.CopyTo(result);
+            return result;
+        }
+
+        public static string[] RemoveDuplicates(string[] strIn)
+        {
+            int numDups = 0, prevIndex = 0;
+            if (null != strIn)
+            {
+                int len = strIn.Length;
+                for (int i = 0; i < len; i++)
+                {
+                    bool foundDup = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (strIn[j] == strIn[i])
+                        {
+                            foundDup = true;
+                            numDups++;
+                            break;
+                        }
+                    }
+
+                    if (foundDup == false)
+                    {
+                        strIn[prevIndex] = strIn[i];
+                        prevIndex++;
+                    }
+                }
+
+                //// Just Duplicate records replce by null. If we can't use another array.
+                //for (int k = 1; k <= numDups; k++)
+                //{
+                //    strIn[len - k] = null;
+                //}
+                //return strIn;
+
+                //If we can use two array then this is the best choice 
+                //without using collection or linq or any api
+                string[] strUnique = new string[len - numDups];
+                Array.Copy(strIn, strUnique, len - numDups);
+                //for (int k = 0; k < len - numDups; k++)
+                //{
+                //    strUnique[k] = strIn[k];
+                //}
+                return strUnique;
+            }
+            return null;
+
+        }
+
+        public static string ReverseWordUsingSplitRev(string str)
+        {
+            string[] arrWord = str.Split(' ');
+            int i = 0;
+            foreach (string word in arrWord)
+            {
+                arrWord[i] = Reverse(word);
+                i++;
+            }
+
+            return string.Join(" ", arrWord);
+        }
+
+        private static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static String ReverseWords(String str)
+        {
+
+            // Pointer to the first character  
+            // of the first word  
+            int start = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                // If the current word has ended  
+                if (str[i] == ' ' ||
+                        i == str.Length - 1)
+                {
+
+                    // Pointer to the last character  
+                    // of the current word  
+                    int end;
+                    if (i == str.Length - 1)
+                        end = i;
+                    else
+                        end = i - 1;
+
+                    // Reverse the current word  
+                    while (start < end)
+                    {
+                        str = swap(str, start, end);
+                        start++;
+                        end--;
+                    }
+
+                    // Pointer to the first character  
+                    // of the next word  
+                    start = i + 1;
+                }
+            }
+
+            return str;
+        }
+
+        static String swap(String str, int i, int j)
+        {
+            char[] ch = str.ToCharArray();
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+            return String.Join("", ch);
+        }
+
+    }
+}
---
+++ CodingProblem/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object Initialization for Employee class
+
+            List<Employee> objEmployee = new List<Employee>{
+                    new Employee{ Name="Sachin",EmpID="I001",Salary=800},
+                    new Employee{ Name="Vijay",EmpID="I002",Salary=400},
+                    new Employee{ Name="Ashish",EmpID="I003",Salary=250},
+                    new Employee{ Name="Syed",EmpID="I004",Salary=300},
+                    new Employee{ Name="Ravish",EmpID="I005",Salary=700},
+                };
+
+            /*
+                QUESTION 1.Difference between ‘this’ and ‘let’ and when to use what, explain with examples.
+
+                The this keyword refers to the current instance of the class 
+                and is also used as a modifier of the first parameter of an extension method.
+
+                The Let keyword allows you to create a range variable and initialized with the result of the query expression 
+                and then you are allowed to use that variable with the upcoming clause in the same query. 
+                When you initialize a range variable with one value after that 
+                you are not allowed to store another value in the range variable.
+            */
+            var objresult = from emp in objEmployee
+                            let totalSalary = objEmployee.Sum(sal => sal.Salary)
+                            let avgSalary = totalSalary / 5
+                            where avgSalary > emp.Salary
+                            select emp;
+
+            /*
+                QUESTION 2.Write a function which accepts an array of strings and returns a map of character to count of the character(including spaces and special characters).
+                Eg: input: [‘hello world’, ‘hello world’]  output: {h: 2, e: 2, l: 6, ‘ ‘: 1, o: 4 …… }
+            */
+            var lstStr = new string[] { "hello world", "hello world", "hello world" };
+            var CharCount = ProblemSolve.AllCharOccurenceCount(lstStr);
+            Console.WriteLine("Occurence of characters in given string array is " + CharCount);
+
+            /*
+                QUESTION 3.Write a function to return all the keys present in an object at any level.
+                Eg: input: {  a: 5, b: { c: { d: 10 } }}   output: [a, b, c, d]
+             */
+            var jsonStr = "{a:5,b:{c:{d:10} }}";
+            var lstKeys = ProblemSolve.GetKeyFromJsonString(jsonStr);
+            Console.WriteLine("The key of the json string " + jsonStr + "is ");
+            Console.Write("[");
+            foreach (var key in lstKeys)
+            {
+                Console.Write(key);
+                Console.Write(" ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+
+            /*
+               QUESTION 4.Write a function to reverse every word of a string.
+                ·         Using split and reverse
+                ·         Without using split and reverse and in O(1) space (since strings are immutable, assume that the input is a character array).
+             */
+
+            var strWords = "Gyana Ranjan Lenka";
+            var revStr = ProblemSolve.ReverseWordUsingSplitRev(strWords);
+            var revStr1 = ProblemSolve.ReverseWords(strWords);
+            Console.WriteLine("The reverse word of " + strWords + " is " + revStr);
+
+            /*
+                QUESTION 5.Write a function to remove duplicate strings from an array of strings.
+             */
+
+            var lstDupStr = new string[] { "hello", "world", "Love", "world", "you", "hello" };
+            var UniqueLst = ProblemSolve.RemoveDuplicates(lstDupStr);
+            Console.WriteLine("The unique word of the string is ");
+            Console.Write("[");
+            foreach (var key in UniqueLst)
+            {
+                Console.Write(key);
+                Console.Write(" ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit!!");
+            Console.ReadLine();
+        }
+    }
+
+    /*
+        QUESTION 1.Difference between ‘this’ and ‘let’ and when to use what, explain with examples.
+
+        The this keyword refers to the current instance of the class 
+        and is also used as a modifier of the first parameter of an extension method.
+    */
+    class Employee
+    {
+        private string _Name;
+        public string _EmpID;
+        public int _Salary;
+        public string Name
+        {
+            get { return _Name; }
+            set { this._Name = value; }
+        }
+        public string EmpID
+        {
+            get { return _EmpID; }
+            set { this._EmpID = value; }
+        }
+        public int Salary
+        {
+            get { return _Salary; }
+            set { this._Salary = value; }
+        }
+    }
+}
